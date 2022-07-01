0e607322edcf53c065f0be04a26896f9db36397e
imalka <imalka@gmail.com>
2022-07-01 13:09:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.use('/rpmt/staff', require('./routers/staff_routes'));
 app.use('/rpmt/supervisor', require('./routers/supervisor_routes'));
 
 
-app.listen(9000, () => {
+app.listen(process.env.PORT || 9000, () => {
     console.log('Server started')
 });
